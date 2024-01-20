a8f79ddd812f0672051910f98c29696ac093c233
Van Husein <muhakbarhusein5@gmail.com>
2024-01-20 09:51:59 +0700

    feat: :tada: FIrst setup

---
+++ .eslintrc.cjs
@@ -0,0 +1,35 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react-hooks/recommended",
+  ],
+  ignorePatterns: ["dist", ".eslintrc.cjs"],
+  parser: "@typescript-eslint/parser",
+  plugins: ["react-refresh"],
+  rules: {
+    "react-refresh/only-export-components": [
+      "warn",
+      { allowConstantExport: true },
+    ],
+    quotes: ["error", "single"],
+    semi: ["error", "never"],
+    "no-unused-vars": "warn",
+    "max-len": [
+      "error",
+      {
+        code: 100,
+        ignoreComments: true,
+        ignoreTrailingComments: true,
+        ignoreUrls: true,
+        ignoreStrings: true,
+        ignoreTemplateLiterals: true,
+        ignoreRegExpLiterals: true,
+      },
+    ],
+    "export/prefer-default-export": "off",
+    "react/jsx-props-no-spreading": "off",
+  },
+};
---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <h1 className='text-center text-3xl font-bold underline'>
+      Hello Universe :)
+    </h1>
+  )
+}
+
+export default App
